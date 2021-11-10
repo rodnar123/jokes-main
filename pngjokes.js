2ac143b96cfa5285adad6ad3d906f5dfd44e87e6
rodnar123 <rodney.naro@gmail.com>
2021-11-10 19:41:59 +1300

    first commit

---
+++ pngjokes.js
@@ -0,0 +1,43 @@
+const pngJokes = [
+  {
+    text: 'Wanla liklik pikni Wara Sepik igo late lo skul na Tisa askim," wai u kam late?". Poiman beki na tok, "Kakaruk sa kirapim mipla lo moning em aste mipla kilim na keke".',
+    author: 'Mariole.Wong.Nauwa',
+  },
+  {
+    text: ' First day blo wanpla home-scholar lo golo skul na avinun em kambek lo aus na mama blem askim," my boi, how was skul na wanem ol pebret subjects bliu?", boiman tok," recess,lunch na holiday"',
+    author: 'Mariole.Wong.Nauwa',
+  },
+  {
+    text: 'Wanpla plesman Sepik bin kadim narpla plesman Chipik wande naip so baga ya pasim kad han plem wande singlet tasol na sixti golo ausik na nurse askm," bikpla blo kat osem wanem?", poiman tok,"kat ya bikpla blem 8 oklok wide na 6 oklok dip"',
+    author: 'Mariole.Wong.Nauwa',
+  },
+  {
+    text: 'Ol health wokman bin igo wokm Family Planning awareness lo wanpla bus ples lo wara Sepik. Apta lo awareness ol wokman distributim wanwan box kondom igo lo ogeta manmeri lo community. Pinis lo disla wanpla lapun papa kambek wantm box kondom na askm opisa," soreh pikini, gumi ya mi paol liklik lo how blo kisim, ba mi dring wanwan or tutu?". (Em ting condom ya wanpla nupla tablet blo dring )',
+    author: 'Mariole.Wong.Nauwa',
+  },
+  {
+    text: 'Wanpla Ameriken Misinery igo kism lotu lo wanpla ples lo Wara Sepik na em painim interpreter so em kism wanpla plesman usat isawer liklik lo english. Taim waitman statim beten blo openim lotu, em pray na tok," Oh fada, you are so compassionate and merciful from generations to generations". Plesman interpreter givim lo side na tok," Oh papa gort, u yet u komputa na mekenik blo ogeta generator igo lo generator"',
+    author: 'Mariole.Wong.Nauwa',
+  },
+  {
+    text: 'Bipo lo Justin Tkatchenko bin kamap memba, em bin godaun lo wara Sepik lo lukim ol Orchids lo hap na ol line lo ples bin welkamim em na kansol like addressim em tasol em paol liklik lo kolim name blo em so em tokm ol people lo ples," Upla oketa kam pung na puti han lo pikpla mangi plo Okid lo PNG em kam lo lukim Okid plo umi lo wara so umi paidi han lem Mr.Chastin.Katnyangwa(Katnangwah)',
+    author: 'Mariole.Wong.Nauwa',
+  },
+  {
+    text: 'Wanpla busman blo Sepik bin go raun lo Aroma cost for two weeks na injet lo disla 2wiks em mas dring 700pla kap tea nambaut. Kain osem 50pla kapti lo wanla day(u sawer ol papua ol line blo dring tea). So time Chava like gobek lo mosbi ol Aroma kam lusim em lo highway na boi nekdrai so ol gim em kolwara lo cup na em kirap kism na winim na dring.( All of 2weeks em dring hot tea everyday ikam na em use pinis lo winim tea na dring so time ol givim kolwara em winim ken na dring osem tea)',
+    author: 'Mariole.Wong.Nauwa',
+  },
+  {
+    text: 'Ol raskol bin holdup lo wanpla bank na leader man blo ol bin karim gun igo putim lo counter na singaut," Hold Up! This is a HandsUp". (Olboi bin pilim fani tasol ol kam lo holdup trutru so ol koporate tasol na kism coins na gobek lo blok na ol mekim fani lo disla igo inap joke ya go viral na polis tracim joke kambek olim ogeta}',
+    author: 'Mariole.Wong.Nauwa',
+  },
+  {
+    text: 'Wanpla man bin baptise wan wik igo pinis na seken week lo em ol raskol bin olim em on his way to church, na ol askm em,"Mani or Laip!?", trangu ya kirap tokm ol raskol," plis mi nogat mani na laip blo mi em mi gim lo bikman laswik yet".',
+    author: 'Mariole.Wong.Nauwa',
+  },
+  {
+    text: 'Wanpla pasto bin kism bible quiz lo sunday skul na em askm ol pikini tok," Mose bin go antap lo Mount Sanai lo kism wanem samting?" wanpla liklik pikini kirap tok," em go antap lo kism Ten Ammendmendts".(Mangia u like reconstitutionalisem baibel?") Pasto bin faint liklik but faith blo em wokm em sanap liklik more inap quiz pinis. ',
+    author: 'Mariole.Wong.Nauwa',
+  },
+  
+];
---
+++ script.js
@@ -0,0 +1,69 @@
+ const quoteContainer = document.getElementById('quote-container');
+ const quoteText = document.getElementById('quote');
+ const authorText = document.getElementById('author');
+ const twitterBtn = document.getElementById('twitter');
+ const newQuoteBtn = document.getElementById('new-quote');
+ const loader = document.getElementById('loader');
+
+ 
+ 
+
+//  Show Loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+    
+}
+
+// Show New Quote
+function newPngJoke() {
+    loading();
+    // Pick a random quote from API array
+    const quote = pngJokes[Math.floor(Math.random()   * pngJokes.length)];
+    // Check if Author field is blank and replace is with 'Unkown!'
+    if (!quote.author) {
+        quoteText.textContent = 'Unkown';
+    } else{
+        authorText.textContent = quote.author;
+    }
+    // Check the quote lenght to determine the styling
+    if(quote.text.length > 120) {
+         quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+    // Set Quote hide loeader
+    quoteText.textContent = quote.text;
+    complete();
+
+}
+
+
+
+
+// Tweet Quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+// Facebook post
+function facebookPost() {
+    const facebookUrl = `https://www.facebook.com/dialog/feed?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(facebookUrl,'_blank');
+}
+
+// Event Listener
+
+newQuoteBtn.addEventListener('click',newPngJoke);
+twitterBtn.addEventListener('click',tweetQuote);
+
+// On laod 
+    
+    newPngJoke();
+
